fc850ecb6eb758e8f019ece98cca526c37398c4e
_Aniketh Srivathsa_ <anikethsrivathsa@gmail.com>
2025-07-07 21:27:06 +0530

    for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './app.jsx'
 import './style.css'  // This line is important!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
