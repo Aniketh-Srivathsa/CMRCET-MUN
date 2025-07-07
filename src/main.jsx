61840086a6293bbe2122866937b3e4774958cb28
_Aniketh Srivathsa_ <anikethsrivathsa@gmail.com>
2025-07-07 21:41:19 +0530

    Temp rename to fix casing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './TempApp.jsx'
 import './style.css'  // This line is important!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
